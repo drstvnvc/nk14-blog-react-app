ba95af885e2f8cdff9cd4d9a907e0daee5692e2c
milos996 <ivankovicmilos5@gmail.com>
2021-08-27 19:51:52 +0200

    Task 2.

--- src/pages/AppPosts.js
+++ src/pages/AppPosts.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import postService from '../services/PostService';
 
 function AppPosts() {
-  return <div>Posts</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const data = await postService.getAll();
+
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div style={{ marginLeft: 5 }}>
+      <h2>My posts</h2>
+      {posts.map((post) => (
+        <div key={post.id} style={{ border: '3px solid orange', width: 300 }}>
+          <p>
+            <strong>Title:</strong> {post.title}
+          </p>
+          <p>
+            <strong>Text:</strong> {post.text}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
 export default AppPosts;
